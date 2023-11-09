8c00957670e31bf00c8b4f349b8923baf9714d92
8kto <ifhtagn@gmail.com>
2023-11-10 00:59:35 +0100

    Autoformat

--- src/components/EncumbranceBadge/EncumbranceBadge.tsx
+++ src/components/EncumbranceBadge/EncumbranceBadge.tsx
@@ -2,7 +2,7 @@ import { useHookstate } from '@hookstate/core'
 import React from 'react'
 
 import { getEncumbrance, getTotal } from '@/components/EncumbranceBadge/helpers'
-import MovementBadge from "@/components/MovementBadge/MovementBadge"
+import MovementBadge from '@/components/MovementBadge/MovementBadge'
 import { EquipmentState } from '@/state/EquipmentState'
 import { combineEquipment } from '@/state/helpers'
 
@@ -10,7 +10,7 @@ const EncumbranceBadge = () => {
   const equipmentState = useHookstate(EquipmentState)
 
   const { totalPoints, totalCost } = getTotal(combineEquipment(equipmentState))
-const encumbrance = getEncumbrance(totalPoints)
+  const encumbrance = getEncumbrance(totalPoints)
   return (
     <ul className='mb-4 list-disc space-y-3 pl-4 text-gray-500 dark:text-gray-400'>
       <li>
@@ -18,7 +18,7 @@ const encumbrance = getEncumbrance(totalPoints)
       </li>
       <li>Total: {totalCost} sp</li>
       <li>
-        <MovementBadge encumbrance={encumbrance}/>
+        <MovementBadge encumbrance={encumbrance} />
       </li>
     </ul>
   )
--- src/components/MovementBadge/MovementBadge.tsx
+++ src/components/MovementBadge/MovementBadge.tsx
@@ -7,13 +7,15 @@ const MovementBadge = ({ encumbrance }: { encumbrance: Encumbrance }) => {
   const movement = MovementMap[encumbrance]
 
   return (
-    <>Movement
-      <ul className='ml-4 mb-4 list-disc space-y-3 pl-4 text-gray-500'>
-        <li>Exploration: {movement["Exploration"]}</li>
-        <li>Combat: {movement["Combat"]}</li>
-        <li>Running: {movement["Running"]}</li>
-        <li>Miles per day: {movement["MilesPerDay"]}</li>
-      </ul></>
+    <>
+      Movement
+      <ul className='mb-4 ml-4 list-disc space-y-3 pl-4 text-gray-500'>
+        <li>Exploration: {movement['Exploration']}</li>
+        <li>Combat: {movement['Combat']}</li>
+        <li>Running: {movement['Running']}</li>
+        <li>Miles per day: {movement['MilesPerDay']}</li>
+      </ul>
+    </>
   )
 }
 
