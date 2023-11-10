293622391f9092a15a8989145b52be728b622331
8kto <ifhtagn@gmail.com>
2023-11-10 22:56:07 +0100

    Rearrange info

--- src/components/EncumbranceFragment/EncumbranceFragment.tsx
+++ src/components/EncumbranceFragment/EncumbranceFragment.tsx
@@ -16,10 +16,10 @@ const EncumbranceFragment = () => {
   const encumbrance = getEncumbrance(totalPoints)
   return (
     <ul className='mb-4 list-disc space-y-3 pl-4 text-gray-500 dark:text-gray-400'>
+      <li>Total: {totalCost} sp</li>
       <li>
         {encumbrance} ({totalPoints})
       </li>
-      <li>Total: {totalCost} sp</li>
       <li>
         <MovementFragment encumbrance={encumbrance} />
       </li>
