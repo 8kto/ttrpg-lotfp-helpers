9b41843a7914943925b51d9a26a1068766443368
8kto <ifhtagn@gmail.com>
2023-11-22 00:04:12 +0100

    Use formik for Add coins form

--- src/components/Inventory/AddCoinsFragment/AddCoinsFragment.tsx
+++ src/components/Inventory/AddCoinsFragment/AddCoinsFragment.tsx
@@ -1,118 +1,113 @@
+/* eslint-disable sort-keys */
 import { XMarkIcon } from '@heroicons/react/24/solid'
-import React, { useState } from 'react'
+import { Field, Form, Formik } from 'formik'
+import React from 'react'
+import * as Yup from 'yup'
 
 import { t } from '@/locale/helpers'
 import { addCopperPieces } from '@/state/InventoryState'
 
 const AddCoinsFragment = ({ onClose }: { onClose: () => void }) => {
-  const [isCopperPieces, setIsCopperPieces] = useState(false)
-  const [coins, setCoins] = useState<number | ''>('')
-
-  const handleCoinsChange: React.ChangeEventHandler<HTMLInputElement> = (
-    event,
-  ) => {
-    const target = event.target as HTMLInputElement
-    if (+target.value) {
-      setCoins(+target.value)
-    }
-  }
-
-  const handleAddCoins = () => {
+  const handleAddCoins = ({
+    isCopper,
+    coins,
+  }: {
+    isCopper: boolean
+    coins: number
+  }) => {
     if (coins) {
-      const amount = isCopperPieces ? coins : coins * 10
+      const amount = isCopper ? coins : coins * 10
       addCopperPieces(amount)
-      setCoins(0)
-      setIsCopperPieces(false)
       onClose()
     }
   }
 
-  const handleFocus: React.FocusEventHandler<HTMLInputElement> = () => {
-    if (coins === 0) {
-      setCoins('') // Clear the input when it's focused with value 0
-    }
-  }
-
-  const handleKeyPress: React.KeyboardEventHandler<HTMLInputElement> = (
-    event,
-  ) => {
-    if (event.key === 'Enter') {
-      handleAddCoins()
-    }
-  }
-
-  // TODO autofocus form fields in all forms
-
   return (
-    <>
-      <h5
-        id='drawer-label--add-coins'
-        className='ph-color-accent mb-6 inline-flex items-center text-2xl'
-      >
-        {t('Add coins')}
-      </h5>
-      <button
-        onClick={onClose}
-        type='button'
-        data-drawer-dismiss='drawer-create-product-default'
-        aria-controls='drawer-create-product-default'
-        className='absolute right-2.5 top-2.5 inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white'
-      >
-        <XMarkIcon className='h-5 w-5' />
-      </button>
-      <div className='space-y-4'>
-        <label
-          htmlFor='add-coins'
-          className='mb-2 block font-medium text-gray-700'
+    <Formik
+      initialValues={{
+        coins: 0,
+        isCopper: false,
+      }}
+      validationSchema={Yup.object({
+        coins: Yup.number()
+          .positive()
+          // .max(9, t('Must be 9 characters max.')) // WTF
+          .required(t('Required')),
+        isCopper: Yup.boolean(),
+      })}
+      onSubmit={(values) => {
+        handleAddCoins(values)
+      }}
+    >
+      {({ handleSubmit }) => (
+        <Form
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault()
+              handleSubmit()
+            }
+          }}
         >
-          {t('Enter coin amount. Use copper pieces instead of float numbers.')}
-        </label>
-        <div className='relative'>
-          <input
-            autoFocus
-            type='number'
-            name='add-coins'
-            id='add-coins'
-            className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-600 focus:ring-primary-600'
-            placeholder='0'
-            value={coins === 0 ? '' : coins}
-            onChange={handleCoinsChange}
-            onFocus={handleFocus}
-            onKeyDown={handleKeyPress}
-          />
-          <span className='absolute inset-y-0 right-3 inline-flex items-center text-base text-gray-400'>
-            {isCopperPieces ? t('CP') : t('SP')}
-          </span>
-        </div>
-        {/* Checkbox */}
-        <div className='flex items-center'>
-          <input
-            checked={isCopperPieces}
-            onChange={() => setIsCopperPieces((v) => !v)}
-            id='copper-coins--add-coins'
-            type='checkbox'
-            value=''
-            className='h-4 w-4 rounded border-gray-300 bg-gray-100 focus:ring-2 focus:ring-blue-500'
-          />
-          <label
-            htmlFor='copper-coins--add-coins'
-            className='ms-2 cursor-pointer text-sm font-medium text-gray-900'
+          <h5
+            id='drawer-label--add-coins'
+            className='ph-color-accent mb-6 inline-flex items-center text-2xl'
           >
-            {t('Copper pieces')}
-          </label>
-        </div>
-
-        <div className='bottom-0 left-0 flex w-full justify-center space-x-4 pb-4 md:absolute md:px-4'>
+            {t('Add coins')}
+          </h5>
           <button
-            onClick={handleAddCoins}
+            onClick={onClose}
             type='button'
-            className='ph-btn-primary w-full justify-center rounded px-5 py-2.5 text-center font-medium focus:outline-none focus:ring-4 focus:ring-primary-300'
+            data-drawer-dismiss='drawer-create-product-default'
+            aria-controls='drawer-create-product-default'
+            className='absolute right-2.5 top-2.5 inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white'
           >
-            {t('Add')}
+            <XMarkIcon className='h-5 w-5' />
           </button>
-        </div>
-      </div>
-    </>
+          <div className='space-y-4'>
+            <label
+              htmlFor='coins'
+              className='mb-2 block font-medium text-gray-700'
+            >
+              {t(
+                'Enter coin amount. Use copper pieces instead of float numbers.',
+              )}
+            </label>
+            <Field
+              autoFocus
+              type='number'
+              className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-600 focus:ring-primary-600'
+              placeholder='0'
+              name='coins'
+              id='coins'
+            />
+
+            {/* Checkbox */}
+            <div className='flex items-center'>
+              <Field
+                type='checkbox'
+                name='isCopper'
+                id='isCopper'
+                className='h-4 w-4 rounded border-gray-300 bg-gray-100 focus:ring-2 focus:ring-blue-500'
+              />
+              <label
+                htmlFor='isCopper'
+                className='ms-2 cursor-pointer text-sm font-medium text-gray-900'
+              >
+                {t('Copper pieces')}
+              </label>
+            </div>
+            <div className='bottom-0 left-0 flex w-full justify-center space-x-4 pb-4 md:absolute md:px-4'>
+              <button
+                type='submit'
+                className='ph-btn-primary w-full justify-center rounded px-5 py-2.5 text-center font-medium focus:outline-none focus:ring-4 focus:ring-primary-300'
+              >
+                {t('Add')}
+              </button>
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
   )
 }
 
