f776daedf55102da2ffc1654e50e5e43037a38bf
8kto <ifhtagn@gmail.com>
2023-11-23 21:57:34 +0100

    Fix encumbrance calculation, fix tests

--- src/components/InventoryDetails/__tests__/helpers.test.ts
+++ src/components/InventoryDetails/__tests__/helpers.test.ts
@@ -2,56 +2,57 @@ import { getEncumbrance, getTotal } from '@/components/InventoryDetails/helpers'
 import type { EquipmentItem, InventoryItem } from '@/domain'
 import { Encumbrance, EncumbrancePoint } from '@/domain/encumbrance'
 
-describe('helpers', () => {
+describe('InventoryDetails helpers', () => {
   describe('getEncumbrance', () => {
     it.each([
       [0, Encumbrance.Unencumbered],
-      [10, Encumbrance.Unencumbered],
-      [11, Encumbrance.Lightly],
-      [15, Encumbrance.Lightly],
-      [16, Encumbrance.Heavily],
-      [20, Encumbrance.Heavily],
-      [21, Encumbrance.Severely],
-      [25, Encumbrance.Severely],
-      [26, Encumbrance.OverEncumbered],
-      [30, Encumbrance.OverEncumbered],
+      [1.8, Encumbrance.Unencumbered],
+      [2, Encumbrance.Lightly],
+      [2.8, Encumbrance.Lightly],
+      [3, Encumbrance.Heavily],
+      [3.8, Encumbrance.Heavily],
+      [4, Encumbrance.Severely],
+      [4.8, Encumbrance.Severely],
+      [5, Encumbrance.OverEncumbered],
+      [100, Encumbrance.OverEncumbered],
     ])('should return %d -> %s', (input, expected) => {
       expect(getEncumbrance(input)).toEqual(expected)
     })
   })
 
   describe('getTotal', () => {
-    it('returns total cost and points of all equipment items', () => {
+    const getItemMock = (lockedCost: number, points: EncumbrancePoint) => {
+      return {
+        cityCost: 100,
+        inventoryId: '1',
+        lockedCost,
+        name: 'Item',
+        points,
+        ruralCost: null,
+      }
+    }
+
+    fit('returns total cost and points of all equipment items', () => {
       const items: InventoryItem<EquipmentItem>[] = [
-        {
-          cityCost: 100,
-          inventoryId: '1',
-          lockedCost: 100,
-          name: 'Sword',
-          points: EncumbrancePoint.Regular,
-          ruralCost: null,
-        },
-        {
-          cityCost: 50,
-          inventoryId: '2',
-          lockedCost: 50,
-          name: 'Shield',
-          points: EncumbrancePoint.Oversized,
-          ruralCost: null,
-        },
-        {
-          cityCost: 200,
-          inventoryId: '3',
-          lockedCost: 200,
-          name: 'Armor',
-          points: EncumbrancePoint.Heavy,
-          ruralCost: null,
-        },
+        getItemMock(200, EncumbrancePoint.None), // skipped as None
+        getItemMock(200, EncumbrancePoint.None), // skipped as None
+        getItemMock(50, EncumbrancePoint.Oversized),
+        getItemMock(200, EncumbrancePoint.None), // skipped as None
+        getItemMock(100, EncumbrancePoint.Regular), // skipped 1st
+        getItemMock(100, EncumbrancePoint.Regular), // skipped 2nd
+        getItemMock(200, EncumbrancePoint.Heavy),
+        getItemMock(100, EncumbrancePoint.Regular), // skipped 3rd
+        getItemMock(100, EncumbrancePoint.Regular), // skipped 4th
+        getItemMock(100, EncumbrancePoint.Regular), // skipped 5th
+        getItemMock(100, EncumbrancePoint.Regular),
+        getItemMock(100, EncumbrancePoint.Regular),
+        getItemMock(50, EncumbrancePoint.Oversized),
+        getItemMock(200, EncumbrancePoint.Heavy),
       ]
 
       expect(getTotal(items)).toEqual({
-        totalCost: 350,
-        totalEncumbrancePoints: 3.2,
+        totalCost: 1800,
+        totalEncumbrancePoints: 6.4,
       })
     })
 
--- src/components/InventoryDetails/helpers.ts
+++ src/components/InventoryDetails/helpers.ts
@@ -3,56 +3,45 @@ import type { ArmorItem } from '@/domain/armor'
 import { Encumbrance, EncumbrancePoint } from '@/domain/encumbrance'
 
 export const getEncumbrance = (points: number): Encumbrance => {
-  // if (points >= 26) {
-  //   return Encumbrance.OverEncumbered
-  // }
-  // if (points >= 21) {
-  //   return Encumbrance.Severely
-  // }
-  // if (points >= 16) {
-  //   return Encumbrance.Heavily
-  // }
-  // if (points >= 11) {
-  //   return Encumbrance.Lightly
-  // }
-
   if (points >= 5) {
     return Encumbrance.OverEncumbered
   }
-  if (points === 4) {
+  if (points >= 4) {
     return Encumbrance.Severely
   }
-  if (points === 3) {
+  if (points >= 3) {
     return Encumbrance.Heavily
   }
-  if (points === 2) {
+  if (points >= 2) {
     return Encumbrance.Lightly
   }
-
-  // FIXME 5 first items are free of encumbrance
 
   return Encumbrance.Unencumbered
 }
 
-const shouldConsiderFactor = (item: InventoryItem<EquipmentItem>): boolean => {
-  return (
-    item.points === EncumbrancePoint.Regular &&
-    !(item as InventoryItem<ArmorItem>).armorClass
-  )
-}
-
 export const getTotal = (items: Array<InventoryItem<EquipmentItem>>) => {
-  // In character list, first 5 items are free of encumbrance
-  // (those that are not of Armor as Oversized items)
-  let factor = -5
+  // The first 5 items in the character list are not subject to encumbrance,
+  // unless they are Armor or Oversized items.
+  let nonEncumberedItemsCount = 5
+  const shouldSkip = (item: InventoryItem<EquipmentItem>): boolean => {
+    const res =
+      nonEncumberedItemsCount > 0 &&
+      item.points === EncumbrancePoint.Regular &&
+      !(item as InventoryItem<ArmorItem>).armorClass
+
+    if (res) {
+      nonEncumberedItemsCount--
+    }
+
+    return res
+  }
 
   return items.reduce(
     (totals, item) => {
       return {
         totalCost: totals.totalCost + item.lockedCost,
         totalEncumbrancePoints:
-          totals.totalEncumbrancePoints +
-          (shouldConsiderFactor(item) && !factor-- ? item.points : 0),
+          totals.totalEncumbrancePoints + (shouldSkip(item) ? 0 : item.points),
       }
     },
     { totalCost: 0, totalEncumbrancePoints: 0 },
