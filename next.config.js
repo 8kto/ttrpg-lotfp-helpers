90ec00a269f188817ad129e73210844af5c0277c
8kto <ifhtagn@gmail.com>
2023-11-25 00:15:20 +0100

    Revert "Install l18n packages"
    This reverts commit 31a9cc8f5ac7a4e336b8a6eb9d1db222f848c3d7.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-/* eslint-disable */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'build',
   output: 'export',
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
-  },
 }
 
 module.exports = nextConfig
