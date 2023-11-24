31a9cc8f5ac7a4e336b8a6eb9d1db222f848c3d7
8kto <ifhtagn@gmail.com>
2023-11-24 23:19:34 +0100

    Install l18n packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+/* eslint-disable */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'build',
   output: 'export',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+  },
 }
 
 module.exports = nextConfig
