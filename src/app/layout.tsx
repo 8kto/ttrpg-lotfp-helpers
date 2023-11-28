5e465b10e225803ec2898094c8a06a29b728ea2c
8kto <ifhtagn@gmail.com>
2023-11-28 23:55:58 +0100

    Display icons, adjust colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className={regularFont.variable}>
+      <head>
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/icons/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/icons/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/icons/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/icons/site.webmanifest' />
+      </head>
       <body className={`${cursiveFont.variable} bg-gray-50 bg-none`}>
         <UiProvider>
           <Header />
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,7 +15,11 @@ const Header = () => {
 
   return (
     <header>
-      <nav className='fixed z-30 w-full border-b border-gray-200 bg-white px-4 py-3'>
+      <nav className='text-white fixed z-30 w-full border-b border-gray-200 px-4 py-3'
+           style={{
+             background: 'url(/images/img-noise-361x370-2.png)',
+           }}
+      >
         <div className='mx-auto flex max-w-screen-2xl items-center justify-between'>
           <div className='flex items-center justify-start'>
             <a href='/' className='mr-14 flex'>
@@ -25,20 +29,6 @@ const Header = () => {
                 Princess Helpers
               </h1>
             </a>
-
-            <div className='hidden w-full items-center justify-between lg:order-1 lg:flex lg:w-auto'>
-              <ul className='mt-4 flex flex-col space-x-6 text-sm font-medium lg:mt-0 lg:flex-row xl:space-x-8'>
-                <li>
-                  <a
-                    href='#'
-                    className='block rounded text-primary-700 dark:text-primary-500'
-                    aria-current='page'
-                  >
-                    Home
-                  </a>
-                </li>
-              </ul>
-            </div>
           </div>
           <div className='flex items-center'>
             <button
