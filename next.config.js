cc9663b833f9373aa8e2d044991b2334a20ed794
8kto <ifhtagn@gmail.com>
2023-11-29 01:44:20 +0100

    Configure locales

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'build',
-  output: 'export',
+  i18n: {
+    defaultLocale: 'default',
+    localeDetection: false,
+    locales: ['default', 'en', 'ru', 'cz'],
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
