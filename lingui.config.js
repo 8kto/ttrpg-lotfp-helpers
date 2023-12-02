db47ed0f0754893e6d93f5cfb1bc0981644bb5da
8kto <ifhtagn@gmail.com>
2023-12-02 19:26:32 +0100

    Set order by message ID

--- lingui.config.js
+++ lingui.config.js
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
   locales: locales,
   sourceLocale: 'en',
+  "orderBy": "messageId",
   pseudoLocale: 'pseudo',
   catalogs: [
     {
