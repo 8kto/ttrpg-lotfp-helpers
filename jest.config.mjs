e460387de5087d0747b53f5d11be1eb5fde5dec0
8kto <ifhtagn@gmail.com>
2023-12-04 21:12:48 +0100

    Fix import aliases

--- jest.config.mjs
+++ jest.config.mjs
@@ -10,8 +10,10 @@ const createJestConfig = nextJest({
 const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
