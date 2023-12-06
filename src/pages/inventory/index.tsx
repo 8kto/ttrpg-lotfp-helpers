2d80f5d0164158e14d780bc098f579f8b2a078b2
8kto <ifhtagn@gmail.com>
2023-12-06 20:44:30 +0100

    Extract Tabs component

--- src/pages/inventory/index.tsx
+++ src/pages/inventory/index.tsx
@@ -5,7 +5,8 @@ import { useLingui } from '@lingui/react'
 import classnames from 'classnames'
 import type { GetStaticProps } from 'next'
 import Head from 'next/head'
-import React, { useContext, useState } from 'react'
+import type { ReactComponentLike } from 'prop-types'
+import React, { useContext, useEffect, useState } from 'react'
 
 import EquipmentList from '@/components/EquipmentList/EquipmentList'
 import InventoryList from '@/components/Inventory/InventoryList'
@@ -22,15 +23,50 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   }
 }
 
-export default function InventoryPage() {
-  const {
-    uiState: { activeTabId },
-  } = useContext(UiContext)
-  const [activeTab, setActiveTab] = useState(activeTabId)
+const Tabs = ({
+  tabs,
+}: {
+  tabs: Array<{
+    title: string
+    component: ReactComponentLike
+  }>
+}) => {
+  const { uiState } = useContext(UiContext)
+  const [activeTabId, setActiveTabId] = useState(uiState.activeTabId)
   const tabTitleBaseClassname =
     'flex-1 py-4 text-xl font-extrabold sm:text-2xl ph-font-cursive'
   const tabTitleActiveClassname = 'border-b-2 border-red-900 text-red-900 '
 
+  // Error handling for out-of-range activeTabId
+  const isValidTabId = activeTabId >= 0 && activeTabId < tabs.length
+  const ActiveTab = isValidTabId ? tabs[activeTabId].component : null
+
+  useEffect(() => {
+    setActiveTabId(uiState.activeTabId)
+  }, [uiState.activeTabId])
+
+  return (
+    <>
+      <div className='flex border-b'>
+        {tabs.map((item, index) => (
+          <button
+            key={item.title}
+            className={classnames(tabTitleBaseClassname, {
+              [tabTitleActiveClassname]: activeTabId === index,
+            })}
+            onClick={() => setActiveTabId(index)}
+          >
+            {item.title}
+          </button>
+        ))}
+      </div>
+
+      {ActiveTab && <ActiveTab />}
+    </>
+  )
+}
+
+export default function InventoryPage() {
   // Subscribe to the locale updates
   useLingui()
 
@@ -43,38 +79,12 @@ export default function InventoryPage() {
         <main className='mx-auto w-full max-w-screen-2xl flex-grow px-4 sm:px-6 lg:px-8'>
           {/* Tabs for smaller screens */}
           <div className='mt-6 bg-white lg:hidden'>
-            <div className='flex border-b'>
-              <button
-                className={classnames(tabTitleBaseClassname, {
-                  [tabTitleActiveClassname]: activeTabId === 0,
-                  'text-gray-500': activeTabId !== 0,
-                })}
-                onClick={() => setActiveTab(0)}
-              >
-                Equipment
-              </button>
-              <button
-                className={classnames(tabTitleBaseClassname, {
-                  [tabTitleActiveClassname]: activeTabId === 1,
-                  'text-gray-500': activeTabId !== 1,
-                })}
-                onClick={() => setActiveTab(1)}
-              >
-                Inventory
-              </button>
-            </div>
-            <div>
-              {activeTab === 0 && (
-                <div className='p-4'>
-                  <EquipmentList />
-                </div>
-              )}
-              {activeTab === 1 && (
-                <div className='p-4'>
-                  <InventoryList />
-                </div>
-              )}
-            </div>
+            <Tabs
+              tabs={[
+                { title: t`Equipment`, component: EquipmentList },
+                { title: t`Inventory`, component: InventoryList },
+              ]}
+            />
           </div>
           {/* Two columns for lg and wider screens */}
           <div className='mt-6 hidden lg:grid lg:grid-cols-2 lg:gap-4 xl:grid-cols-3'>
@@ -92,3 +102,4 @@ export default function InventoryPage() {
 }
 
 // TODO match viewport programmatically no to render components twice
+// FIXME this above should also fix the broken labels for City/Rural cost
