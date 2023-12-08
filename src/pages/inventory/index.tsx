4c32306f308f140b0b0dad66aa8e62da801403ca
8kto <ifhtagn@gmail.com>
2023-12-08 21:19:55 +0100

    Set wider content for mobiles

--- src/pages/inventory/index.tsx
+++ src/pages/inventory/index.tsx
@@ -86,7 +86,7 @@ export default function InventoryPage() {
         <title>{t`Inventory`}</title>
       </Head>
       <div className='relative flex min-h-screen flex-col bg-gray-50 pt-16'>
-        <main className='mx-auto w-full max-w-screen-2xl flex-grow px-4 sm:px-6 lg:px-8'>
+        <main className='mx-auto w-full max-w-screen-2xl flex-grow px-2.5 sm:px-6 lg:px-8'>
           {shouldRenderTabs ? (
             /* Tabs for smaller screens */
             <div className='ph-inventory-page-content--tabs mt-6 rounded border border-gray-200 bg-white lg:hidden'>
