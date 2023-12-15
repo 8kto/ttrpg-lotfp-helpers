7d5621b7f66b4640ef53bb8bdeb283d1d3c2bf8d
8kto <ifhtagn@gmail.com>
2023-12-15 01:24:21 +0100

    Add script for updating tasks stats

---
+++ scripts/update_readme_stats.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the file name
+FILE="README.md"
+
+# Count total tasks (both completed and uncompleted)
+total_tasks=$(grep -oP "\- \[\s?x?\]" "$FILE" | wc -l)
+completed_tasks=$(grep -oP "\- \[x\]" "$FILE" | wc -l)
+
+# Calculate the percentage of completed tasks
+if [ $total_tasks -ne 0 ]; then
+    percentage=$((100 * completed_tasks / total_tasks))
+else
+    percentage=0
+fi
+
+echo "$percentage% - done $completed_tasks of $total_tasks tasks"
+
+# Update the file by replacing content between <!--PROGRESS--> and <!--/PROGRESS-->
+sed -i "s/<!--PROGRESS-->.*<!--\/PROGRESS-->/<!--PROGRESS--> $percentage% - done $completed_tasks of $total_tasks tasks <!--\/PROGRESS-->/" "$FILE"
+
