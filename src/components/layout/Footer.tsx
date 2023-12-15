6643fd15a0700da912c78950d55c5aa1280698d1
8kto <ifhtagn@gmail.com>
2023-12-15 01:14:35 +0100

    Fix width for footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='relative mx-auto w-full max-w-screen-2xl bg-gray-50 px-4 py-6 md:flex md:items-center md:justify-between md:py-10 2xl:px-0'>
+    <footer className='relative mx-auto w-full max-w-screen-2xl bg-gray-50 px-4 py-6 md:flex md:items-center md:justify-between md:py-10 2xl:px-8'>
       <p className='mb-4 text-center text-gray-500 md:mb-0'>
         © 2023{' '}
         <a
