7c6c8f00de148707374838b6d6bae47edbffc2e9
8kto <ifhtagn@gmail.com>
2023-12-16 17:18:07 +0100

    Extend tests

--- src/components/Inventory/ItemDetails/__tests__/ItemDetails.test.tsx
+++ src/components/Inventory/ItemDetails/__tests__/ItemDetails.test.tsx
@@ -1,16 +1,39 @@
 import '@testing-library/jest-dom'
 
-import { render, screen } from '@testing-library/react'
+import { i18n } from '@lingui/core'
+import { I18nProvider } from '@lingui/react'
+import { act, render, screen } from '@testing-library/react'
 import React from 'react'
 
 import ItemDetails from '@/components/Inventory/ItemDetails/ItemDetails'
+import { ArmorType } from '@/domain/armor'
+import { EncumbrancePoint } from '@/domain/encumbrance'
+
+const TestingProvider = ({ children }: { children: React.ReactNode }) => (
+  <I18nProvider i18n={i18n}>{children}</I18nProvider>
+)
 
 describe('<ItemDetails />', () => {
   const mockItem = {
-    name: 'Test Item',
+    armorClass: 14,
+    cityCost: 25,
+    inventoryId: '1',
+    lockedCost: 50,
+    name: 'Leather',
+    points: EncumbrancePoint.None,
+    ruralCost: 50,
+    type: ArmorType.Armor,
     qty: 2,
     details: 'Some details about the item',
   }
+
+  i18n.load('en', {})
+  i18n.activate('en')
+  beforeEach(() => {
+    act(() => {
+      i18n.activate('en')
+    })
+  })
 
   it('renders item name', () => {
     render(
@@ -18,8 +41,9 @@ describe('<ItemDetails />', () => {
         // @ts-ignore
         item={mockItem}
       />,
+      { wrapper: TestingProvider },
     )
-    expect(screen.getByText('Test Item')).toBeInTheDocument()
+    expect(screen.getByText('Leather')).toBeVisible()
   })
 
   it('renders quantity fragment for items with qty > 1', () => {
@@ -28,8 +52,20 @@ describe('<ItemDetails />', () => {
         // @ts-ignore
         item={mockItem}
       />,
+      { wrapper: TestingProvider },
     )
-    expect(screen.getByTestId('item-qty')).toBeInTheDocument()
+    expect(screen.getByTestId('item-qty')).toBeVisible()
+  })
+
+  it('does not render quantity fragment for items with qty = 1', () => {
+    render(
+      <ItemDetails
+        // @ts-ignore
+        item={{ ...mockItem, qty: 1 }}
+      />,
+      { wrapper: TestingProvider },
+    )
+    expect(screen.queryByTestId('item-qty')).not.toBeInTheDocument()
   })
 
   it('renders item details line', () => {
@@ -39,8 +75,10 @@ describe('<ItemDetails />', () => {
         item={mockItem}
         compact={false}
       />,
+      { wrapper: TestingProvider },
     )
     expect(screen.getByText('Some details about the item')).toBeInTheDocument()
+    expect(screen.getByText('Some details about the item')).not.toBeVisible()
   })
 
   it('does not render details block when showDetailsBlock is false', () => {
@@ -50,6 +88,7 @@ describe('<ItemDetails />', () => {
         item={mockItem}
         showDetailsBlock={false}
       />,
+      { wrapper: TestingProvider },
     )
     const details = screen.queryByText('Some details about the item')
     expect(details).not.toBeVisible()
@@ -62,7 +101,46 @@ describe('<ItemDetails />', () => {
         item={mockItem}
         showDetailsBlock={true}
       />,
+      { wrapper: TestingProvider },
     )
     expect(screen.getByText('Some details about the item')).toBeInTheDocument()
+    expect(screen.getByText('Some details about the item')).not.toBeVisible()
+  })
+
+  it('renders item name in compact mode', () => {
+    render(
+      <ItemDetails
+        // @ts-ignore
+        item={mockItem}
+        compact={true}
+      />,
+      { wrapper: TestingProvider },
+    )
+    expect(screen.getByText('Leather')).toBeInTheDocument()
+  })
+
+  it('renders quantity fragment in compact mode for items with qty > 1', () => {
+    render(
+      <ItemDetails
+        // @ts-ignore
+        item={mockItem}
+        compact={true}
+      />,
+      { wrapper: TestingProvider },
+    )
+    expect(screen.getByTestId('item-qty')).toBeVisible()
+  })
+
+  it('does not render item details line in compact mode', () => {
+    render(
+      <ItemDetails
+        // @ts-ignore
+        item={mockItem}
+        compact={true}
+      />,
+      { wrapper: TestingProvider },
+    )
+    const details = screen.queryByText('Some details about the item')
+    expect(details).not.toBeVisible()
   })
 })
