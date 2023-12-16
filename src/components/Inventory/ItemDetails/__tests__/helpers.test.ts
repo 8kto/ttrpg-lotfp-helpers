a297e1ff227ed09a564b4a55e5d8a6a80573b152
8kto <ifhtagn@gmail.com>
2023-12-16 16:20:53 +0100

    Cover ItemDetails helpers

---
+++ src/components/Inventory/ItemDetails/__tests__/helpers.test.ts
@@ -0,0 +1,57 @@
+import {
+  isArmorItem,
+  isInventoryItem,
+  isMissileItem,
+  isWeaponItem,
+} from '@/components/Inventory/ItemDetails/helpers'
+import {
+  armorItemMock1,
+  meleeWeaponItemMock1,
+  miscEquipItem1,
+  missileWeaponItemMock1,
+  missileWeaponItemMock2,
+} from '@/shared/mocks/inventoryMocks'
+
+describe('ItemDetails helpers', () => {
+  describe('isArmorItem', () => {
+    it('should return true', () => {
+      expect(isArmorItem(armorItemMock1)).toBeTruthy()
+    })
+    it('should return false', () => {
+      expect(isArmorItem(miscEquipItem1)).toBeFalsy()
+    })
+  })
+
+  describe('isWeaponItem', () => {
+    it('should return true', () => {
+      expect(isWeaponItem(missileWeaponItemMock2)).toBeTruthy()
+      expect(isWeaponItem(meleeWeaponItemMock1)).toBeTruthy()
+    })
+    it('should return false', () => {
+      expect(isArmorItem(miscEquipItem1)).toBeFalsy()
+    })
+  })
+
+  describe('isMissileItem', () => {
+    it('should return true', () => {
+      expect(isMissileItem(missileWeaponItemMock1)).toBeTruthy()
+    })
+    it('should return false', () => {
+      expect(isArmorItem(miscEquipItem1)).toBeFalsy()
+    })
+  })
+
+  describe('isInventoryItem', () => {
+    it('should return true', () => {
+      expect(isInventoryItem(armorItemMock1)).toBeTruthy()
+      expect(isInventoryItem(missileWeaponItemMock1)).toBeTruthy()
+      expect(isInventoryItem(meleeWeaponItemMock1)).toBeTruthy()
+      expect(isInventoryItem(miscEquipItem1)).toBeTruthy()
+    })
+    it('should return false', () => {
+      const mock = { ...miscEquipItem1 }
+      delete mock.qty
+      expect(isInventoryItem(mock)).toBeFalsy()
+    })
+  })
+})
