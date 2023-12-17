fd7cc219ca9c0dcd3b0dfdc19112f8786b2e3463
8kto <ifhtagn@gmail.com>
2023-12-17 20:27:55 +0100

    Introduce ResetFormOnDrawerClose component, that handles resetting the formik context

---
+++ src/components/Inventory/ResetFormOnDrawerClose.tsx
@@ -0,0 +1,26 @@
+import { useFormikContext } from 'formik'
+import React, { useEffect } from 'react'
+
+import Action from '@/shared/actions/actions'
+import { subscribe } from '@/shared/actions/helpers'
+
+/**
+ * A component that provides a workaround to reset a Formik form when a DrawerClose event occurs,
+ * without unmounting the form.
+ */
+export const ResetFormOnDrawerClose = ({
+  onDrawerClose,
+}: {
+  onDrawerClose: CallableFunction
+}) => {
+  const formikContext = useFormikContext()
+
+  useEffect(() => {
+    return subscribe(Action.DrawerClose, () => {
+      formikContext.resetForm()
+      onDrawerClose()
+    })
+  }, [onDrawerClose, formikContext])
+
+  return <React.Fragment />
+}
