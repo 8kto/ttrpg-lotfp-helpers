c0ba62f075635ec7d2ed3aa30155ba9c44985d02
8kto <ifhtagn@gmail.com>
2023-12-19 14:02:12 +0100

    Extract util

--- src/components/InventoryDetails/helpers.ts
+++ src/components/InventoryDetails/helpers.ts
@@ -1,32 +1,14 @@
+import { Coin } from '@/domain'
 import type { ArmorItem } from '@/domain/armor'
 import { EncumbrancePoint } from '@/domain/encumbrance'
-import type { EquipmentItem } from '@/domain/equipment'
 import type { InventoryItem } from '@/domain/inventory'
-import { COINS_PER_ENCUMBRANCE_POINT } from '@/shared/helpers/encumbrance'
+import type { CountableItem } from '@/shared/helpers/encumbrance'
+import { getCoinItems } from '@/shared/helpers/encumbrance'
 import { roundTo } from '@/shared/helpers/roundTo'
 
-type CountableItem = Pick<
-  InventoryItem<EquipmentItem>,
-  'points' | 'lockedCost' | 'name' | 'qty'
->
-
-const getCoinInventoryItems = (copperPieces: number) => {
-  const coins = copperPieces / 10
-  const coinsEncumbrance = Math.floor(coins / COINS_PER_ENCUMBRANCE_POINT)
-
-  return Array.from({ length: coinsEncumbrance }, () => {
-    return {
-      lockedCost: 0,
-      name: '100 coins',
-      points: EncumbrancePoint.Regular,
-      qty: 1,
-    }
-  })
-}
-
 /**
- * The first 5 items in the character list are not subject to encumbrance,
- * unless they are Armor or Oversized items.
+ * The first X (5 for regular characters, 10 for dwarves) items in the character list
+ * are not subject to encumbrance, unless they are Armor or Oversized items.
  */
 const skipFirstItems = () => {
   let nonEncumberedItemsCount = 5
@@ -48,7 +30,7 @@ const skipFirstItems = () => {
 export const getTotal = (items: Array<CountableItem>, copperPieces: number) => {
   // FIXME pass over the remained num e.g. -2
   const shouldSkip = skipFirstItems()
-  const records = items.concat(getCoinInventoryItems(copperPieces))
+  const records = items.concat(getCoinItems(copperPieces, Coin.Copper))
 
   const res = records.reduce(
     (totals, item) => {
