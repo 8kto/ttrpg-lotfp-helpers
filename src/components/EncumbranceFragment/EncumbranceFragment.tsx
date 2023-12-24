c838259161d743ce77152a607dc7c64fe6f4ce57
8kto <ifhtagn@gmail.com>
2023-12-24 15:28:36 +0100

    Render toggle for enc threshold

--- src/components/EncumbranceFragment/EncumbranceFragment.tsx
+++ src/components/EncumbranceFragment/EncumbranceFragment.tsx
@@ -1,8 +1,15 @@
+import { t, Trans } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import React from 'react'
 
+import Toggle from '@/components/Toggle/Toggle'
+import { EncumbranceThreshold } from '@/domain/encumbrance'
 import isDevEnv from '@/shared/helpers/isDevEnv'
 import EncumbranceService from '@/shared/services/EncumbranceService'
+import {
+  setFreeEncumbranceSlots,
+  useInventoryState,
+} from '@/state/InventoryState'
 
 const EncumbranceFragment = ({
   encumbrancePoints,
@@ -11,26 +18,44 @@ const EncumbranceFragment = ({
 }) => {
   const { _: trans } = useLingui()
   const encumbrance = EncumbranceService.getEncumbrance(encumbrancePoints)
+  const {
+    state: { freeEncumbranceSlots },
+  } = useInventoryState()
+
+  const toggleEncumbranceThreshold = () => {
+    setFreeEncumbranceSlots(
+      freeEncumbranceSlots.get() === EncumbranceThreshold.Regular
+        ? EncumbranceThreshold.Dwarf
+        : EncumbranceThreshold.Regular,
+    )
+  }
 
   // TODO plurals for feet
   // TODO modal with FAQ about encumbrance
   return (
-    <>
-      {trans(encumbrance)}{' '}
-      {isDevEnv() ? (
-        <span className='text-gray-400'>
-          ({encumbrancePoints.toPrecision(2)} e.u. /{' '}
-          {EncumbranceService.getReadableEncumbrance(encumbrancePoints)} e.p.)
-        </span>
-      ) : (
-        <>
+    <div className='flex flex-col'>
+      <div>
+        {trans(encumbrance)}{' '}
+        {isDevEnv() ? (
+          <span className='text-gray-400'>
+            ({encumbrancePoints.toPrecision(2)} e.u. /{' '}
+            {EncumbranceService.getReadableEncumbrance(encumbrancePoints)} e.p.)
+          </span>
+        ) : (
           <span className='text-gray-400'>
             ({EncumbranceService.getReadableEncumbrance(encumbrancePoints)}{' '}
             e.p.)
           </span>
-        </>
-      )}
-    </>
+        )}
+      </div>
+      <Toggle
+        checked={freeEncumbranceSlots.get() === EncumbranceThreshold.Dwarf}
+        onChange={toggleEncumbranceThreshold}
+        title={t`Include the extended encumbrance ability.`}
+      >
+        <Trans>Dwarf encumbrance</Trans>
+      </Toggle>
+    </div>
   )
 }
 
