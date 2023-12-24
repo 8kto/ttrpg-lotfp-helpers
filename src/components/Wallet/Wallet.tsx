ca185244f72c1c51147e528c0fc49df26c22b429
8kto <ifhtagn@gmail.com>
2023-12-24 15:18:11 +0100

    Use Toggle component

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react'
 import CostFragment from '@/components/CostFragment/CostFragment'
 import Drawer from '@/components/Drawer/Drawer'
 import SetCoinsFragment from '@/components/Inventory/SetCoinsFragment/SetCoinsFragment'
+import Toggle from '@/components/Toggle/Toggle'
 import {
   resetCurrencies,
   toggleCoinsWeightActive,
@@ -15,8 +16,10 @@ const Wallet = () => {
   const [isManaged, setIsManaged] = useState(false)
   const [isSetCoinsDrawerOpen, setSetCoinsDrawerOpen] = useState(false)
 
-  const { state } = useInventoryState()
-  const { wallet, isCoinWeightActive } = state
+  // FIXME doesnt affect displayed encumbrance
+  const {
+    state: { wallet, isCoinWeightActive },
+  } = useInventoryState()
 
   return (
     <div className='px-0'>
@@ -43,6 +46,7 @@ const Wallet = () => {
       </div>
       {/* 2nd row */}
       <>
+        {/* TODO enable this toggle */}
         <label className='relative inline-flex hidden cursor-pointer items-center'>
           <input
             type='checkbox'
@@ -60,21 +64,13 @@ const Wallet = () => {
             <Trans>Manage</Trans>
           </span>
         </label>
-        <label className='relative inline-flex cursor-pointer items-center'>
-          <input
-            type='checkbox'
-            checked={isCoinWeightActive.get()}
-            className='peer sr-only'
-            onChange={() => toggleCoinsWeightActive()}
-          />
-          <div className="peer h-6	w-11 scale-90 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-red-900 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-red-300"></div>
-          <span
-            className='ms-2'
-            title={t`Enable the weight of coins for encumbrance calculations.`}
-          >
-            <Trans>Include the weight</Trans>
-          </span>
-        </label>
+        <Toggle
+          checked={isCoinWeightActive.get()}
+          onChange={toggleCoinsWeightActive}
+          title={t`Enable the weight of coins for encumbrance calculations.`}
+        >
+          <Trans>Include the weight</Trans>
+        </Toggle>
       </>
     </div>
   )
