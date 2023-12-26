4c5f441050f4975c47cef950687f884c2add9f66
8kto <ifhtagn@gmail.com>
2023-12-26 20:50:55 +0100

    Fix the SSR hydration issues when the active tab is retored from the URL

--- src/pages/inventory/index.tsx
+++ src/pages/inventory/index.tsx
@@ -6,10 +6,11 @@ import classnames from 'classnames'
 import type { GetStaticProps } from 'next'
 import Head from 'next/head'
 import type { ReactComponentLike } from 'prop-types'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import EquipmentList from '@/components/EquipmentList/EquipmentList'
 import InventoryList from '@/components/Inventory/InventoryList'
+import Spinner from '@/components/Spinner'
 import UiContext from '@/shared/context/uiContext'
 import useTailwindBreakpoint from '@/shared/hooks/useTailwindBreakpoint'
 import { loadCatalog } from '@/translations/utils'
@@ -37,17 +38,34 @@ const Tabs = ({
     updateUiState,
   } = useContext(UiContext)
   const tabTitleBaseClassname =
-    'flex-1 py-4 text-xl font-extrabold sm:text-2xl ph-font-cursive hover:border-red-500 hover:text-red-800 border-b-2'
+    'flex-1 py-4 text-xl font-extrabold sm:text-2xl ph-font-cursive hover:border-red-500 hover:text-red-800 border-b-2 outline-transparent focus-visible:border-b-transparent'
   const tabTitleActiveClassname = 'border-b-2 border-red-900 text-red-900 '
+
+  // Fix the SSR hydration quirks
+  const [isMounted, setIsMounted] = useState(false)
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return (
+      <>
+        <Spinner />
+      </>
+    )
+  }
 
   return (
     <>
-      <div className='flex border-b'>
+      <div className='flex border-b' role='tablist'>
         {tabs.map((item, index) => (
           <button
+            role='tab'
             key={item.title}
+            suppressHydrationWarning
             className={classnames(tabTitleBaseClassname, {
               [tabTitleActiveClassname]: activeTabId === index,
+              'ph-active-tab': index === activeTabId,
             })}
             onClick={() => updateUiState({ activeTabId: index })}
           >
@@ -58,8 +76,11 @@ const Tabs = ({
 
       {tabs.map(({ component: Component, title }, index) => (
         <div
+          role='tabpanel'
+          suppressHydrationWarning
           className={classnames('p-4', {
             hidden: index !== activeTabId,
+            'ph-active-tabpanel': index === activeTabId,
           })}
           key={title}
         >
