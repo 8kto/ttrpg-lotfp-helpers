7e7ef9aab632d36113a488a9274174f7a59a629d
8kto <ifhtagn@gmail.com>
2023-12-26 20:51:27 +0100

    Add role attrs

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -18,6 +18,7 @@ type TabsProps = {
 
 const Tab = ({ active, onClick, children }: TabProps) => (
   <button
+    role='tab'
     className={`group inline-flex items-center justify-center rounded-t-lg border-b-2 p-4 hover:border-red-500 hover:text-red-800 outline-transparent focus-visible:border-b-transparent ${
       active
         ? 'active border-red-900 text-red-900 hover:border-red-900 hover:text-red-900'
@@ -38,6 +39,7 @@ const Tabs = ({ tabs }: TabsProps) => {
         <nav
           className='-mb-px flex flex-wrap text-center font-medium text-gray-500 dark:text-gray-400'
           aria-label='Tabs'
+          role='tablist'
         >
           {tabs.map((tab) => (
             <Tab
@@ -52,7 +54,11 @@ const Tabs = ({ tabs }: TabsProps) => {
       </div>
       {tabs.map(
         (tab) =>
-          activeTab === tab.key && <div key={tab.key}>{tab.content}</div>,
+          activeTab === tab.key && (
+            <div key={tab.key} role='tabpanel'>
+              {tab.content}
+            </div>
+          ),
       )}
     </>
   )
