cb37aa7b2684e9b251845952c6481ffd62b9872a
8kto <ifhtagn@gmail.com>
2023-12-26 19:48:49 +0100

    Track UI context state in the URL hash params

--- src/shared/context/UiContextProvider.tsx
+++ src/shared/context/UiContextProvider.tsx
@@ -1,16 +1,28 @@
 'use client'
 import React, { useState } from 'react'
 
+import useUiContextFromHashParams from '@/shared/hooks/useUiContextFromHashParams'
+
 import type { UiContextType } from './uiContext'
 import UiContext, { defaultUiState } from './uiContext'
 
 const UiProvider = ({ children }: { children: React.ReactNode }) => {
-  const [uiState, setUiState] = useState<UiContextType['uiState']>(
-    defaultUiState.uiState,
-  )
+  const uiContextFromHashParams = useUiContextFromHashParams()
+  const [uiState, setUiState] = useState<UiContextType['uiState']>({
+    ...defaultUiState.uiState,
+    ...uiContextFromHashParams,
+  })
 
   const updateUiState = (newState: Partial<UiContextType['uiState']>) => {
-    setUiState((state) => ({ ...state, ...newState }))
+    setUiState((state) => {
+      const merged = { ...state, ...newState }
+
+      window.location.hash = new URLSearchParams(
+        merged as unknown as Record<string, string>,
+      ).toString()
+
+      return merged
+    })
   }
 
   return (
