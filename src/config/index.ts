6f6a3619580d91de80528d16c95c488ceda924ed
8kto <ifhtagn@gmail.com>
2023-12-26 22:29:18 +0100

    Split up the domain and translation info

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,12 @@ export { ArmorItems } from './ArmorItems'
 export { MeleeWeaponItems } from './MeleeWeaponItems'
 export { MiscEquipmentItems } from './MiscEquipmentItems'
 export { MissileWeaponItems } from './MissileWeaponItems'
+
+export const Labels = {
+  Cumbersome: /*i18n*/ 'Cumbersome',
+  Heavily: /*i18n*/ 'Heavily',
+  Lightly: /*i18n*/ 'Lightly',
+  OverEncumbered: /*i18n*/ 'OverEncumbered',
+  Severely: /*i18n*/ 'Severely',
+  Unencumbered: /*i18n*/ 'Unencumbered',
+}
--- src/domain/encumbrance.ts
+++ src/domain/encumbrance.ts
@@ -1,18 +1,17 @@
 export enum EncumbranceUnit {
-  /*i18n*/ None = 0,
-  /*i18n*/ Regular = 0.2,
-  /*i18n*/ Oversized = 1,
-  /*i18n*/ Heavy = 2,
-  /*i18n*/ Cumbersome = 3,
+  None = 0,
+  Regular = 0.2,
+  Oversized = 1,
+  Heavy = 2,
+  Cumbersome = 3,
 }
 
 export enum Encumbrance {
-  Unencumbered = /*i18n*/ 'Unencumbered',
-  Lightly = /*i18n*/ 'Lightly',
-  Heavily = /*i18n*/ 'Heavily',
-  Severely = /*i18n*/ 'Severely',
-  OverEncumbered = /*i18n*/ 'OverEncumbered',
-  Cumbersome = /*i18n*/ 'Cumbersome',
+  Unencumbered = 'Unencumbered',
+  Lightly = 'Lightly',
+  Heavily = 'Heavily',
+  Severely = 'Severely',
+  OverEncumbered = 'OverEncumbered',
 }
 
 // NB Oversized is as tall as Character
