a5a3dd1e4024b619aff80a1589ee21ca412dd858
8kto <ifhtagn@gmail.com>
2023-12-27 18:38:58 +0100

    Adjust script to parse only the features list

--- scripts/update_readme_stats.sh
+++ scripts/update_readme_stats.sh
@@ -3,9 +3,12 @@
 # Define the file name
 FILE="README.md"
 
-# Count total tasks (both completed and uncompleted)
-total_tasks=$(grep -oP "\- \[\s?x?\]" "$FILE" | wc -l)
-completed_tasks=$(grep -oP "\- \[x\]" "$FILE" | wc -l)
+# Extract the section between <!--FEATURES_LIST--> and <!--/FEATURES_LIST-->
+section=$(sed -n '/<!--FEATURES_LIST-->/, /<!--\/FEATURES_LIST-->/p' "$FILE")
+
+# Count total tasks (both completed and uncompleted) within the extracted section
+total_tasks=$(echo "$section" | grep -oP "\- \[\s?x?\]" | wc -l)
+completed_tasks=$(echo "$section" | grep -oP "\- \[x\]" | wc -l)
 
 # Calculate the percentage of completed tasks
 if [ $total_tasks -ne 0 ]; then
@@ -18,4 +21,3 @@ echo "$percentage% - done $completed_tasks of $total_tasks tasks"
 
 # Update the file by replacing content between <!--PROGRESS--> and <!--/PROGRESS-->
 sed -i "s/<!--PROGRESS-->.*<!--\/PROGRESS-->/<!--PROGRESS--> $percentage% \/ $completed_tasks of $total_tasks tasks <!--\/PROGRESS-->/" "$FILE"
-
