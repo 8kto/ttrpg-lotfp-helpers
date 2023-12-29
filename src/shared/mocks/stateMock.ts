bae556bb0123c42caa27edb1e4801bc2b0a16c6f
8kto <ifhtagn@gmail.com>
2023-12-29 22:18:15 +0100

    Extract state mock

---
+++ src/shared/mocks/stateMock.ts
@@ -0,0 +1,27 @@
+import { EncumbranceThreshold } from '@/domain/encumbrance'
+import {
+  armorItemMock1,
+  armorItemMock2,
+  meleeWeaponItemMock1,
+  meleeWeaponItemMock2,
+  miscEquipItem1,
+  miscEquipItem2,
+  missileWeaponItemMock1,
+  missileWeaponItemMock2,
+} from '@/shared/mocks/inventoryMocks'
+import type { InventoryStateType } from '@/state/InventoryState'
+
+export const stateMock: InventoryStateType = Object.freeze({
+  armor: [armorItemMock1, armorItemMock2],
+  encumbranceThreshold: EncumbranceThreshold.Regular,
+  isCoinWeightActive: true,
+  isCostRural: false,
+  meleeWeapons: [meleeWeaponItemMock1, meleeWeaponItemMock2],
+  miscEquipment: [miscEquipItem1, miscEquipItem2],
+  missileWeapons: [missileWeaponItemMock1, missileWeaponItemMock2],
+  wallet: {
+    Copper: 10,
+    Gold: 25,
+    Silver: 31,
+  },
+})
