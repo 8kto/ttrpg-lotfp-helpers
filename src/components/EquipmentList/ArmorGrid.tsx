41af8690d2b10c0b620a7b95da49146b878c7132
8kto <ifhtagn@gmail.com>
2024-01-06 00:04:58 +0100

    Suppress linter while there is no better solution

--- src/components/EquipmentList/ArmorGrid.tsx
+++ src/components/EquipmentList/ArmorGrid.tsx
@@ -81,6 +81,7 @@ const ArmorGrid = () => {
         : data
     },
     // NB! Don't remove i18nContext dep, since it causes the grid rerender on locale change
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [isCostRural, i18nContext],
   )
 
--- src/components/EquipmentList/MiscEquipmentGrid.tsx
+++ src/components/EquipmentList/MiscEquipmentGrid.tsx
@@ -73,6 +73,7 @@ const MiscEquipmentGrid = () => {
         : data
     },
     // NB! Don't remove i18nContext dep, since it causes the grid rerender on locale change
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [isCostRural, i18nContext],
   )
 
