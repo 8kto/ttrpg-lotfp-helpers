f89401d2c7af93b750ec90e5084f7cfe916e6c91
8kto <ifhtagn@gmail.com>
2024-01-10 00:08:37 +0100

    Use DataGrid

--- src/components/Inventory/ArmorInventoryGrid.tsx
+++ src/components/Inventory/ArmorInventoryGrid.tsx
@@ -1,22 +1,21 @@
 import { t } from '@lingui/macro'
 import React from 'react'
 
-import { renderNameInventoryGridCol } from '@/components/EquipmentList/gridHelpers'
-import InventoryGrid from '@/components/Inventory/InventoryGrid'
-import type { InventoryColumn } from '@/components/Inventory/types'
+import DataGrid from '@/components/DataGrid/DataGrid'
+import type { DataGridColumn } from '@/components/DataGrid/types'
+import { renderDetailsBody } from '@/components/EquipmentList/gridHelpers'
 import type { ArmorItem } from '@/domain/armor'
 import type { InventoryItem } from '@/domain/inventory'
 import { removeArmor, useInventoryState } from '@/state/InventoryState'
 
 type ArmorInventoryItem = InventoryItem<ArmorItem>
 
-const inventoryTableColumns: ReadonlyArray<
-  InventoryColumn<ArmorInventoryItem>
-> = [
+const columns: ReadonlyArray<DataGridColumn<ArmorInventoryItem>> = [
   {
     className: 'w-1/2 sm:w-1/3',
     key: 'name',
-    render: renderNameInventoryGridCol,
+    shouldRenderDetails: (item) => !!item.details,
+    renderDetails: renderDetailsBody,
     get title() {
       return t`Name`
     },
@@ -37,10 +36,16 @@ const ArmorInventoryGrid = () => {
   const onRemoveClick = (item: ArmorInventoryItem) => removeArmor(item)
 
   return (
-    <InventoryGrid<ArmorInventoryItem>
+    <DataGrid<ArmorInventoryItem>
       data={armor.get()}
-      columns={inventoryTableColumns}
+      columns={columns}
       onRemoveClick={onRemoveClick}
+      spanDetails={columns.length}
+      initialSortState={{
+        key: '' as keyof ArmorInventoryItem,
+        direction: 'asc',
+      }}
+      noFilter
     />
   )
 }
