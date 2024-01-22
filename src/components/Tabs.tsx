95ebcb9765d18959866901dc838410adba639bf3
8kto <ifhtagn@gmail.com>
2024-01-23 00:41:44 +0100

    Add hover styles for scrollbar

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -19,29 +19,45 @@ type TabsProps = {
   onTabClick: (index: number) => void
 }
 
-const Tab = ({ active, onClick, children }: TabProps) => (
-  <button
-    role='tab'
-    className={classnames(
-      'group inline-flex items-center justify-center p-4',
-      'hover:ph-color-accent outline-gray-200 focus-visible:border-b-transparent',
-      {
-        'active ph-color-accent rounded-tl rounded-tr border': active,
-        'bg-gray-50 text-gray-500 hover:text-red-800': !active,
-      },
-    )}
-    onClick={onClick}
-  >
-    {children}
-  </button>
-)
+const Tab = ({ active, onClick, children }: TabProps) => {
+  const handleDragStart = () => {
+    const scrollbar = document.querySelector('.ph-tabs-scrollbar')
+    scrollbar?.classList.add('active-scrollbar')
+  }
+
+  const handleDragEnd = () => {
+    const scrollbar = document.querySelector('.ph-tabs-scrollbar')
+    scrollbar?.classList.remove('active-scrollbar')
+  }
+
+  return (
+    <button
+      role='tab'
+      className={classnames(
+        'group inline-flex items-center justify-center p-4',
+        'text-sm font-medium text-gray-500 md:text-base',
+        'hover:ph-color-accent outline-gray-200 focus-visible:border-b-transparent',
+        {
+          'active ph-color-accent rounded-tl rounded-tr border': active,
+          'bg-gray-50 text-gray-500 hover:text-red-800': !active,
+        },
+      )}
+      onClick={onClick}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      draggable='true'
+    >
+      {children}
+    </button>
+  )
+}
 
 const Tabs = ({ tabs, onTabClick, activeTabId }: TabsProps) => {
   return (
     <>
-      <div className='ph-tabs-scrollbar overflow-x-auto border-b border-gray-200'>
+      <div className='ph-tabs-scrollbar overflow-x-auto md:border-b md:border-gray-200'>
         <nav
-          className='-mb-px flex flex-nowrap whitespace-nowrap text-center font-medium text-gray-500'
+          className='-mb-px flex flex-nowrap whitespace-nowrap'
           aria-label='Tabs'
           role='tablist'
         >
