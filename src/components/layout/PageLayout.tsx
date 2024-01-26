8d92cba59fe0f8ee7837904603c652ab8c9a341d
8kto <ifhtagn@gmail.com>
2024-01-26 18:14:20 +0100

    Fit to the screen height

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -8,7 +8,7 @@ const PageLayout = ({ children }: { children: React.ReactNode }) => {
       <div className='relative flex flex-grow flex-col pt-16'>
         <main className='ph-content-container mx-auto mb-4 w-full max-w-screen-md flex-grow px-2.5 sm:px-6 lg:px-8'>
           <div className='mt-6'>
-            <div className='col-span-1 rounded border border-gray-200 bg-white p-4 shadow-sm sm:p-6'>
+            <div className='min-h-[calc(100vh-16rem)] col-span-1 rounded border border-gray-200 bg-white p-4 shadow-sm sm:p-6'>
               {children}
             </div>
           </div>
