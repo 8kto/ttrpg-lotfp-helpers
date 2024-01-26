07826089954c561a731c66f51d2fecbd4c04cef1
8kto <ifhtagn@gmail.com>
2024-01-26 01:08:43 +0100

    Update texts, add links

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -25,7 +25,7 @@ export default function AboutPage() {
         <title>{t`About`}</title>
       </Head>
       <div className='relative flex flex-grow flex-col pt-16'>
-        <main className='mx-auto w-full max-w-screen-md flex-grow px-2.5 sm:px-6 lg:px-8'>
+        <main className='mx-auto mb-4 w-full max-w-screen-md flex-grow px-2.5 sm:px-6 lg:px-8'>
           <div className='mt-6'>
             <div className='col-span-1 rounded border border-gray-200 bg-white p-4 shadow-sm sm:p-6'>
               <section className='mb-6'>
@@ -35,20 +35,33 @@ export default function AboutPage() {
                   </h2>
                 </header>
                 <p>
-                  Unadjusted AC is that of solely the armor and shield.
-                  Dexterity modifiers, magical modifiers, or any other
-                  adjustments are not counted. Unadjusted AC is that of solely
-                  the armor and shield. Dexterity modifiers, magical modifiers,
-                  or any other adjustments are not counted. Unadjusted AC is
-                  that of solely the armor and shield. Dexterity modifiers,
-                  magical modifiers, or any other adjustments are not counted.
-                  Unadjusted AC is that of solely the armor and shield.
-                  Dexterity modifiers, magical modifiers, or any other
-                  adjustments are not counted. Unadjusted AC is that of solely
-                  the armor and shield. Dexterity modifiers, magical modifiers,
-                  or any other adjustments are not counted. Unadjusted AC is
-                  that of solely the armor and shield. Dexterity modifiers,
-                  magical modifiers, or any other adjustments are not counted.
+                  This project is created just for fun and is not officially
+                  affiliated with or endorsed by the publishers of
+                  &quot;Lamentations of the Flame Princess&quot; (LotFP) or any
+                  other related entities. The content utilized in this project
+                  has been sourced from publicly available materials, including
+                  but not limited to the &quot;LotFP Rules & Magic Free
+                  Version&quot; available at{' '}
+                  <a
+                    href='https://preview.drivethrurpg.com/en/product/115059/LotFP-Rules--Magic-Free-Version'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='ph-color-accent hover:underline underline-offset-2 '
+                  >
+                    DriveThruRPG
+                  </a>{' '}
+                  and resources from &quot;Basic Fantasy RPG&quot; found at{' '}
+                  <a
+                    href='https://www.basicfantasy.org/downloads.html'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='ph-color-accent hover:underline underline-offset-2 '
+                  >
+                    Basic Fantasy
+                  </a>
+                  . This project is intended for personal entertainment and
+                  educational purposes only, and no commercial benefit is
+                  derived from its use.
                 </p>
               </section>
               <section className='mb-6'>
@@ -57,19 +70,7 @@ export default function AboutPage() {
                     Encumbrance
                   </h2>
                 </header>
-                Unadjusted AC is that of solely the armor and shield. Dexterity
-                modifiers, magical modifiers, or any other adjustments are not
-                counted. Unadjusted AC is that of solely the armor and shield.
-                Dexterity modifiers, magical modifiers, or any other adjustments
-                are not counted. Unadjusted AC is that of solely the armor and
-                shield. Dexterity modifiers, magical modifiers, or any other
-                adjustments are not counted. Unadjusted AC is that of solely the
-                armor and shield. Dexterity modifiers, magical modifiers, or any
-                other adjustments are not counted. Unadjusted AC is that of
-                solely the armor and shield. Dexterity modifiers, magical
-                modifiers, or any other adjustments are not counted. Unadjusted
-                AC is that of solely the armor and shield. Dexterity modifiers,
-                magical modifiers, or any other adjustments are not counted.
+                <p>The encumbrance points and slots are explained.</p>
               </section>
               <section className='mb-6'>
                 <header>
@@ -77,19 +78,15 @@ export default function AboutPage() {
                     Feedback, bug reports, features requests
                   </h2>
                 </header>
-                Unadjusted AC is that of solely the armor and shield. Dexterity
-                modifiers, magical modifiers, or any other adjustments are not
-                counted. Unadjusted AC is that of solely the armor and shield.
-                Dexterity modifiers, magical modifiers, or any other adjustments
-                are not counted. Unadjusted AC is that of solely the armor and
-                shield. Dexterity modifiers, magical modifiers, or any other
-                adjustments are not counted. Unadjusted AC is that of solely the
-                armor and shield. Dexterity modifiers, magical modifiers, or any
-                other adjustments are not counted. Unadjusted AC is that of
-                solely the armor and shield. Dexterity modifiers, magical
-                modifiers, or any other adjustments are not counted. Unadjusted
-                AC is that of solely the armor and shield. Dexterity modifiers,
-                magical modifiers, or any other adjustments are not counted.
+                <p>
+                  My webpage: <a href='https://ivlev.blog/'>ivlev.blog</a>
+                </p>
+                <p>
+                  GitHub:{' '}
+                  <a href='https://github.com/8kto/ttrpg-lotfp-helpers'>
+                    Princess Helpers
+                  </a>
+                </p>
               </section>
             </div>
           </div>
