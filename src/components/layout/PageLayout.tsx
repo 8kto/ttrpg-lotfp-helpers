bd16eb7b8d8c982abfc6bfb3bf5c6af808d3bbf8
8kto <ifhtagn@gmail.com>
2024-01-26 18:05:30 +0100

    Extract common layout into a component

---
+++ src/components/layout/PageLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Toast from '@/components/Toast/Toast'
+
+const PageLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <div className='relative flex flex-grow flex-col pt-16'>
+        <main className='ph-content-container mx-auto mb-4 w-full max-w-screen-md flex-grow px-2.5 sm:px-6 lg:px-8'>
+          <div className='mt-6'>
+            <div className='col-span-1 rounded border border-gray-200 bg-white p-4 shadow-sm sm:p-6'>
+              {children}
+            </div>
+          </div>
+        </main>
+      </div>
+      <Toast />
+    </>
+  )
+}
+
+export default PageLayout
