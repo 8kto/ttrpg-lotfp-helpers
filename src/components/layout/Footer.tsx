5fff52912acc6dde14427ae5b811e2468d1d4b0b
8kto <ifhtagn@gmail.com>
2024-01-26 18:01:07 +0100

    Extract component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,34 +1,19 @@
-import { Trans } from '@lingui/macro'
-import classnames from 'classnames'
 import Image from 'next/image'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import React, { useState } from 'react'
 
+import NavigationLinks from '@/components/layout/NavigationLinks'
 import isDevEnv from '@/shared/helpers/isDevEnv'
 
 const Footer = () => {
   const [viewportDebugHidden, setViewportDebugHidden] = useState(false)
-  const pathname = usePathname()
-  const isAboutPageActive = pathname === '/about'
 
   return (
     <footer className='text-base text-gray-700'>
       <div className='mx-auto max-w-screen-2xl px-4 py-6 sm:px-6 lg:px-8'>
         <div className='flex flex-col md:flex-row md:items-center md:justify-between'>
           <div className='mb-2 text-base md:mb-0'>
-            <Link
-              href='/about'
-              className={classnames(
-                'ph-color-accent mr-4 sm:mr-6 underline-offset-4',
-                {
-                  'font-bold underline': isAboutPageActive,
-                  'hover:underline': !isAboutPageActive,
-                },
-              )}
-            >
-              <Trans>About</Trans>
-            </Link>
+            <NavigationLinks />
             <Link
               href='https://github.com/8kto/ttrpg-lotfp-helpers'
               target='_blank'
