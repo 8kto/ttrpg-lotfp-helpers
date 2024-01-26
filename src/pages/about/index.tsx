47cd81e5e467bc0c73fe95c288b2abbde8aee306
8kto <ifhtagn@gmail.com>
2024-01-26 01:23:29 +0100

    Extend texts

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -25,65 +25,77 @@ export default function AboutPage() {
         <title>{t`About`}</title>
       </Head>
       <div className='relative flex flex-grow flex-col pt-16'>
-        <main className='mx-auto mb-4 w-full max-w-screen-md flex-grow px-2.5 sm:px-6 lg:px-8'>
+        <main className='ph-content-container mx-auto mb-4 w-full max-w-screen-md flex-grow px-2.5 sm:px-6 lg:px-8'>
           <div className='mt-6'>
             <div className='col-span-1 rounded border border-gray-200 bg-white p-4 shadow-sm sm:p-6'>
-              <section className='mb-6'>
+              <section>
                 <header>
-                  <h2 className='ph-color-primary mb-2 text-xl text-gray-900 sm:text-2xl'>
-                    About
-                  </h2>
+                  <h2>Disclaimer</h2>
                 </header>
                 <p>
                   This project is created just for fun and is not officially
                   affiliated with or endorsed by the publishers of
                   &quot;Lamentations of the Flame Princess&quot; (LotFP) or any
-                  other related entities. The content utilized in this project
-                  has been sourced from publicly available materials, including
-                  but not limited to the &quot;LotFP Rules & Magic Free
-                  Version&quot; available at{' '}
+                  other related entities.
+                </p>
+
+                <p>
+                  The content utilized in this project has been sourced from
+                  publicly available materials, including but not limited to the{' '}
                   <a
                     href='https://preview.drivethrurpg.com/en/product/115059/LotFP-Rules--Magic-Free-Version'
                     target='_blank'
                     rel='noopener noreferrer'
-                    className='ph-color-accent hover:underline underline-offset-2 '
                   >
-                    DriveThruRPG
+                    LotFP Rules & Magic Free Version
                   </a>{' '}
-                  and resources from &quot;Basic Fantasy RPG&quot; found at{' '}
+                  and resources from{' '}
                   <a
                     href='https://www.basicfantasy.org/downloads.html'
                     target='_blank'
                     rel='noopener noreferrer'
-                    className='ph-color-accent hover:underline underline-offset-2 '
                   >
-                    Basic Fantasy
+                    Basic Fantasy RPG
                   </a>
-                  . This project is intended for personal entertainment and
-                  educational purposes only, and no commercial benefit is
-                  derived from its use.
+                  .
                 </p>
               </section>
-              <section className='mb-6'>
+              <section>
                 <header>
-                  <h2 className='ph-color-primary mb-2 text-xl text-gray-900 sm:text-2xl'>
-                    Encumbrance
-                  </h2>
+                  <h2>About</h2>
+                </header>
+                <p>
+                  This is an online inventory and equipment generator for the
+                  LotFP system.
+                </p>
+              </section>
+              <section>
+                <header>
+                  <h2>Encumbrance</h2>
                 </header>
                 <p>The encumbrance points and slots are explained.</p>
               </section>
-              <section className='mb-6'>
+              <section>
                 <header>
-                  <h2 className='ph-color-primary mb-2 text-xl text-gray-900 sm:text-2xl'>
-                    Feedback, bug reports, features requests
-                  </h2>
+                  <h2>Feedback, bug reports, features requests</h2>
                 </header>
                 <p>
-                  My webpage: <a href='https://ivlev.blog/'>ivlev.blog</a>
+                  My webpage:{' '}
+                  <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://ivlev.blog/'
+                  >
+                    ivlev.blog
+                  </a>
                 </p>
                 <p>
                   GitHub:{' '}
-                  <a href='https://github.com/8kto/ttrpg-lotfp-helpers'>
+                  <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://github.com/8kto/ttrpg-lotfp-helpers'
+                  >
                     Princess Helpers
                   </a>
                 </p>
