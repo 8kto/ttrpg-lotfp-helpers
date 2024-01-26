9425414a13d8c4c4f47d2986e9304c410eb5a0f8
8kto <ifhtagn@gmail.com>
2024-01-26 18:47:25 +0100

    Revert "Center only large screens"
    This reverts commit f1074db9815a1a0b77cc69076787c2ebce7d9948.

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -5,16 +5,15 @@ import Toast from '@/components/Toast/Toast'
 const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className='flex flex-col pt-16 lg:justify-center lg:min-h-[calc(100vh-5rem)] min-h-[calc(100vh-6rem)]'>
-        <main className='ph-content-container mx-auto mb-4 w-full max-w-screen-md px-2.5 sm:px-6 lg:px-8'>
+      <div className='relative flex flex-grow flex-col pt-16'>
+        <main className='ph-content-container mx-auto mb-4 w-full max-w-screen-md flex-grow px-2.5 sm:px-6 lg:px-8'>
           <div className='mt-6'>
-            <div className='min-h-[calc(100vh-16rem)] lg:min-h-0 col-span-1 rounded border border-gray-200 bg-white p-4 shadow-sm sm:p-6'>
+            <div className='min-h-[calc(100vh-16rem)] col-span-1 rounded border border-gray-200 bg-white p-4 shadow-sm sm:p-6'>
               {children}
             </div>
           </div>
         </main>
       </div>
-
       <Toast />
     </>
   )
