7ef250b1d6146e43ea255a2b3f472e58b1486a29
8kto <ifhtagn@gmail.com>
2024-01-26 18:43:50 +0100

    Adjust styles, make the content be centered on mobiles

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -5,12 +5,10 @@ import Toast from '@/components/Toast/Toast'
 const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className='relative flex flex-grow flex-col pt-16'>
-        <main className='ph-content-container mx-auto mb-4 w-full max-w-screen-md flex-grow px-2.5 sm:px-6 lg:px-8'>
-          <div className='mt-6'>
-            <div className='col-span-1 rounded border border-gray-200 bg-white p-4 shadow-sm sm:p-6'>
-              {children}
-            </div>
+      <div className='flex flex-grow lg:items-center pt-[5.125rem] md:pt-16'>
+        <main className='ph-content-container mx-auto w-full max-w-screen-md px-2.5 sm:px-6 lg:px-8'>
+          <div className='rounded border border-gray-200 bg-white p-4 shadow-sm sm:p-6'>
+            {children}
           </div>
         </main>
       </div>
