259c9a02be4d1880c5a7d9f9653104964cf21acb
8kto <ifhtagn@gmail.com>
2024-02-19 23:47:45 +0100

    Rearrange meta tags in Head, add PWA meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,30 +6,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/icons/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='64x64'
-          href='/icons/favicon-64x64.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/icons/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/icons/favicon-16x16.png'
-        />
-        <link rel='manifest' href='/icons/site.webmanifest' />
+        <meta
+          name='viewport'
+          content='initial-scale=1, viewport-fit=cover, width=device-width'
+        ></meta>
       </Head>
       <body className={`${cursiveFont.variable} ${regularFont.variable}`}>
         <Main />
