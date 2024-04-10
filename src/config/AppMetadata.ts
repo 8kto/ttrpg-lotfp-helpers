26b5e5a52935d1355a13677977a043e7a59c7d91
8kto <ifhtagn@gmail.com>
2024-04-10 23:48:08 +0200

    Adjust description

--- src/config/AppMetadata.ts
+++ src/config/AppMetadata.ts
@@ -8,7 +8,7 @@ export const PROJECT_URL = 'https://github.com/8kto/ttrpg-lotfp-helpers'
 
 export const AppMetadata: Metadata = {
   get description() {
-    return t`Lamentations of the Flame Princess helpers`
+    return t`Lamentations of the Flame Princess helpers, LotFP generator, LotFP equipment, LotFP inventory`
   },
 
   get title() {
