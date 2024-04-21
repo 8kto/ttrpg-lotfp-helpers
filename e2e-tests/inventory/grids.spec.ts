fc64dd209bd188f56325ecd10e8db57380000659
8kto <ifhtagn@gmail.com>
2024-04-21 22:14:47 +0200

    Add sortings tests for all grids

--- e2e-tests/inventory/grids.spec.ts
+++ e2e-tests/inventory/grids.spec.ts
@@ -1,8 +1,31 @@
 import type { Page } from '@playwright/test'
 import { expect, test } from '@playwright/test'
 
+type TabName = 'Armor' | 'Melee' | 'Missiles' | 'Miscellaneous'
+
+type TabDef = {
+  name: TabName
+  testId: string
+}
+
+type GridHeaderDef = {
+  index: number
+  name: string
+}
+
+const tabDefs: Record<TabName, TabDef> = {
+  Armor: { name: 'Armor', testId: 'Tab-armor' },
+  Melee: { name: 'Melee', testId: 'Tab-melee' },
+  Missiles: { name: 'Missiles', testId: 'Tab-missile' },
+  Miscellaneous: { name: 'Miscellaneous', testId: 'Tab-miscEquipment' },
+}
+
 test.describe('Equipment Grid Tests', () => {
-  async function checkFirstItem(
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/inventory')
+  })
+
+  async function testFirstGridCell(
     page: Page,
     tabTestId: string,
     expectedText: string,
@@ -14,71 +37,126 @@ test.describe('Equipment Grid Tests', () => {
     await expect(firstItemText).toHaveText(expectedText)
   }
 
-  test('tabs should properly switch and display correct grids', async ({ page }) => {
-    await page.goto('/inventory')
-    await expect(page.getByTestId('CommandBar')).toBeVisible()
-
-    await checkFirstItem(page, 'Tab-armor', 'Chain')
-    await checkFirstItem(page, 'Tab-melee', 'Battle axe')
-    await checkFirstItem(page, 'Tab-missile', 'Arrow (1)')
-    await checkFirstItem(page, 'Tab-miscEquipment', 'Air Bladder')
-  })
-
-  test('should sort rows', async ({
+  test('tabs should properly switch and display correct grids', async ({
     page,
   }) => {
-    await page.goto('/inventory')
+    await expect(page.getByTestId('CommandBar')).toBeVisible()
 
-    const headers = [
-      { name: 'AC', index: 2, type: 'number' },
-      { name: 'Cost, sp', index: 3, type: 'number' },
-      // FIXME sort by weight values not titles
-      // { name: 'Weight', index: 4, type: 'string' },
-      { name: 'Name', index: 1, type: 'string' },
-    ]
+    await testFirstGridCell(page, tabDefs.Armor.testId, 'Chain')
+    await testFirstGridCell(page, tabDefs.Melee.testId, 'Battle axe')
+    await testFirstGridCell(page, tabDefs.Missiles.testId, 'Arrow (1)')
+    await testFirstGridCell(page, tabDefs.Miscellaneous.testId, 'Air Bladder')
+  })
 
-    for (const header of headers) {
-      // Click the header to sort by this column in ascending order
-      await page.click(
-        `table[data-testid="DataGrid__Table"] >> th:nth-child(${header.index})`,
+  const testGridSorting = async (
+    page: Page,
+    header: Record<string, unknown> & GridHeaderDef,
+  ) => {
+    // Click the header to sort by this column in ascending order
+    await page.click(
+      `table[data-testid="DataGrid__Table"] >> th:nth-child(${header.index})`,
+    )
+
+    // Grab text values of the first rows in this column to check sorting
+    const values = await page.$$eval(
+      `table[data-testid="DataGrid__Table"] >> tbody > tr > td:nth-child(${header.index})`,
+      (tds: Array<HTMLElement>) => tds.map((td) => td.textContent?.trim()),
+    )
+
+    // Sort values based on their type
+    let sortedValues
+    if (header.type === 'number') {
+      sortedValues = [...values].sort((a, b) => parseFloat(a!) - parseFloat(b!))
+    } else {
+      sortedValues = [...values].sort((a, b) => a!.localeCompare(b!))
+    }
+    expect(values).toEqual(sortedValues)
+
+    // click again to sort in descending order and check
+    await page.click(
+      `table[data-testid="DataGrid__Table"] >> th:nth-child(${header.index})`,
+    )
+    const valuesDesc = await page.$$eval(
+      `table[data-testid="DataGrid__Table"] >> tbody > tr > td:nth-child(${header.index})`,
+      (tds: Array<HTMLElement>) => tds.map((td) => td.textContent?.trim()),
+    )
+
+    // Sort values in descending order based on their type
+    let sortedValuesDesc
+    if (header.type === 'number') {
+      sortedValuesDesc = [...valuesDesc].sort(
+        (a, b) => parseFloat(b!) - parseFloat(a!),
       )
+    } else {
+      sortedValuesDesc = [...valuesDesc].sort((a, b) => b!.localeCompare(a!))
+    }
+    expect(valuesDesc).toEqual(sortedValuesDesc)
+  }
 
-      // Grab text values of the first rows in this column to check sorting
-      const values = await page.$$eval(
-        `table[data-testid="DataGrid__Table"] >> tbody > tr > td:nth-child(${header.index})`,
-        (tds: Array<HTMLElement>) => tds.map((td) => td.textContent?.trim()),
-      )
+  test('should sort rows in Armor grid', async ({ page }) => {
+    const gridHeaders: ReadonlyArray<Record<string, unknown> & GridHeaderDef> =
+      [
+        { name: 'AC', index: 2, type: 'number' },
+        { name: 'Cost, sp', index: 3, type: 'number' },
+        // FIXME sort by weight values not titles
+        // { name: 'Weight', index: 4, type: 'string' },
+        { name: 'Name', index: 1, type: 'string' },
+      ]
 
-      // Sort values based on their type
-      let sortedValues
-      if (header.type === 'number') {
-        sortedValues = [...values].sort(
-          (a, b) => parseFloat(a!) - parseFloat(b!),
-        )
-      } else {
-        sortedValues = [...values].sort((a, b) => a!.localeCompare(b!))
-      }
-      expect(values).toEqual(sortedValues)
+    for (const header of gridHeaders) {
+      await testGridSorting(page, header)
+    }
+  })
 
-      // click again to sort in descending order and check
-      await page.click(
-        `table[data-testid="DataGrid__Table"] >> th:nth-child(${header.index})`,
-      )
-      const valuesDesc = await page.$$eval(
-        `table[data-testid="DataGrid__Table"] >> tbody > tr > td:nth-child(${header.index})`,
-        (tds: Array<HTMLElement>) => tds.map((td) => td.textContent?.trim()),
-      )
+  test('should sort rows in Melee grid', async ({ page }) => {
+    await page.getByTestId(tabDefs.Melee.testId).click()
 
-      // Sort values in descending order based on their type
-      let sortedValuesDesc
-      if (header.type === 'number') {
-        sortedValuesDesc = [...valuesDesc].sort(
-          (a, b) => parseFloat(b!) - parseFloat(a!),
-        )
-      } else {
-        sortedValuesDesc = [...valuesDesc].sort((a, b) => b!.localeCompare(a!))
-      }
-      expect(valuesDesc).toEqual(sortedValuesDesc)
+    const gridHeaders: ReadonlyArray<Record<string, unknown> & GridHeaderDef> =
+      [
+        { name: 'Damage', index: 2, type: 'number' },
+        { name: 'Cost, sp', index: 3, type: 'number' },
+        // FIXME sort by weight values not titles
+        // { name: 'Weight', index: 4, type: 'string' },
+        { name: 'Name', index: 1, type: 'string' },
+      ]
+
+    for (const header of gridHeaders) {
+      await testGridSorting(page, header)
+    }
+  })
+
+  test('should sort rows in Missiles grid', async ({ page }) => {
+    await page.getByTestId(tabDefs.Missiles.testId).click()
+
+    const gridHeaders: ReadonlyArray<Record<string, unknown> & GridHeaderDef> =
+      [
+        { name: 'Damage', index: 2, type: 'number' },
+        // FIXME sort by range
+        // { name: 'Range', index: 3, type: 'string' },
+        { name: 'Cost, sp', index: 4, type: 'number' },
+        // FIXME sort by weight values not titles
+        // { name: 'Weight', index: 4, type: 'string' },
+        { name: 'Name', index: 1, type: 'string' },
+      ]
+
+    for (const header of gridHeaders) {
+      await testGridSorting(page, header)
+    }
+  })
+
+  test('should sort rows in Miscellaneous grid', async ({ page }) => {
+    await page.getByTestId(tabDefs.Miscellaneous.testId).click()
+
+    const gridHeaders: ReadonlyArray<Record<string, unknown> & GridHeaderDef> =
+      [
+        { name: 'Cost, sp', index: 2, type: 'number' },
+        // FIXME sort by weight values not titles
+        // { name: 'Weight', index: 4, type: 'string' },
+        { name: 'Name', index: 1, type: 'string' },
+      ]
+
+    for (const header of gridHeaders) {
+      await testGridSorting(page, header)
     }
   })
 })
