11b026ee7a7fb20bde76a23921baaa4d8280e458
8kto <ifhtagn@gmail.com>
2024-05-01 00:37:15 +0200

    Standardize year periods

--- src/domain/firearms.ts
+++ src/domain/firearms.ts
@@ -4,13 +4,36 @@ export enum FiringMechanism {
   Flintlock = 'Flintlock',
 }
 
-export const CoeffYear = {
+export enum YearPeriod {
+  '1610-1630' = '1610-1630',
+  '1631-1660' = '1631-1660',
+  '> 1661' = '> 1661',
+}
+
+type PeriodCoefficients = {
+  [period in YearPeriod]?: number
+} & {
+  default: number
+}
+
+type CoeffYearType = {
+  [key in FiringMechanism]: PeriodCoefficients
+}
+
+export const CoeffYear: CoeffYearType = {
   [FiringMechanism.Flintlock]: {
-    '1610-1630': 2,
-    '1631-1660': 1.5,
+    [YearPeriod['1610-1630']]: 2,
+    [YearPeriod['1631-1660']]: 1.5,
+    [YearPeriod['> 1661']]: 1,
+    default: 2,
   },
-  default: 1,
-} as const
+  [FiringMechanism.Matchlock]: {
+    default: 1,
+  },
+  [FiringMechanism.Wheellock]: {
+    default: 7,
+  },
+}
 
 export const FirearmCostCoeff = {
   [FiringMechanism.Matchlock]: 1,
